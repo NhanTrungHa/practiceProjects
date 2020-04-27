72bab773b9cb61db584712f886269a573160287b
Nhan Ha <nhantrungha@gmail.com>
2020-04-26 18:13:02 -0700

    made madlib file reader

--- madLibs.py
+++ madLibs.py
@@ -2,8 +2,29 @@
 # madLibs.py
 # Usage: Reads a text file and replaces word types with user input.
 
-# Open a file in write mode
-# Create regexes for possible word types
+import re
+
+# Opens file in read mode and store words into variable
+madFile = open(f'madLib.txt', 'r')
+words = madFile.read().split()
+madFile.close()
+
+# Open result file in write mode
+madFile = open(f'madLibResult.txt', 'w')
+# Create regex for possible word types
+adjReg = re.compile(r'ADJECTIVE')
+nounReg = re.compile(r'NOUN')
+verbReg = re.compile(r'VERB')
+
 # loop through each word and compare to regex
-# if regex matches then prompt user
-# after looping through entire text file print and save and close the file
+for word in words:
+    if adjReg.match(word):
+        word = input("Enter an adjective: ")
+    elif nounReg.match(word):
+        word = input("Enter a noun: ")
+    elif verbReg.match(word):
+        word = input("Enter a verb: ")
+# Create variable for printing and save to file
+    madLibPrint = word + " "
+    madFile.write(word + " ")
+madFile.close()
--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -1,5 +1,5 @@
 #! python3
-#! randomQuizGenerator.py
+# randomQuizGenerator.py
 
 import random
 
