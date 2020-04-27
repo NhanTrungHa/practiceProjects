39f550da798a95ba14f5a0469658f5d7bcc051fe
Nhan Ha <nhantrungha@gmail.com>
2020-04-26 17:53:01 -0700

    Create madLibs.py

---
+++ madLibs.py
@@ -0,0 +1,9 @@
+#! python3
+# madLibs.py
+# Usage: Reads a text file and replaces word types with user input.
+
+# Open a file in write mode
+# Create regexes for possible word types
+# loop through each word and compare to regex
+# if regex matches then prompt user
+# after looping through entire text file print and save and close the file
