5e51116d87e0a671cd0172d9cddc795af20f5a91
Nhan Ha <nhantrungha@gmail.com>
2020-04-29 17:26:15 -0700

    Update renameDates.py

---
+++ renameDates.py
@@ -0,0 +1,27 @@
+#! python3
+# renameDates.py - Renames american to european dates.
+
+
+import shutil, os, re
+
+datePattern = re.compile(r"""^(.*?)
+                        (([01])\d)-
+                        (([0123])\d)-
+                        ((19|20)\d\d)
+                        (.*?)$
+""", re.VERBOSE)
+
+# Loop through files
+for amerFileName in os.listdir('.'):
+    mo = datePattern.search(amerFileName)
+
+    if mo is None:
+        continue
+
+    beforePart = mo.group(1)
+    monthPart = mo.group(2)
+    dayPart = mo.group(3)
+    yearPart = mo.group(4)
+    afterPart = mo.group(5)
+
+# Form European filename.
