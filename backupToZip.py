e7f75d22d86e4c160f45675d921fd717833cc10e
Nhan Ha <nhantrungha@gmail.com>
2020-04-29 18:24:46 -0700

    Update backupToZip.py

--- backupToZip.py
+++ backupToZip.py
@@ -11,4 +11,20 @@ def backupToZip(folder):
         zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
         if not os.path.exists(zipFilename):
             break
+        number += 1
 
+    print(f'Creating {zipFilename}...')
+    backupZip = zipfile.ZipFile(zipFilename, 'w')
+
+    for foldername, subfolders, filenames in os.walk(folder):
+        print(f'Adding files in {foldername}')
+        backupToZip.write(foldername)
+
+        for filename in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue
+            backupToZip.write(os.path.join(foldername, filename))
+        backupZip.close()
+
+        print('Done.')
