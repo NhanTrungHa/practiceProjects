df4264c95e1e6ff78d889c82e51b7e7636b7931d
Nhan Ha <nhantrungha@gmail.com>
2020-04-30 19:41:23 -0700

    Update gapFiller.py

--- gapFiller.py
+++ gapFiller.py
@@ -7,10 +7,30 @@ import re
 
 def gapFiller(folder, prefix):
     folder = os.path.abspath(folder)
-    fileReg = re.compile(r'%s\d\d\d\.txt' % prefix)
+    fileReg = re.compile(r'(%s)(\d\d\d)\.txt' % prefix)
 
-    while True:
-        basefiles = os.path.basename(folder) + '_' + str(number) + '.zip'
-        if not os.path.exists(zipFilename):
-            break
-        number += 1
+    # store all file with prefex
+    prefixFiles = list()
+    # loop through the list and see if there is gap
+    for foldername, subfolders, filenames in os.walk(folder):
+        for filename in filenames:
+            if re.search(fileReg, filename):
+                prefixFiles.append(filename)
+
+    prefixFiles.sort()
+    # create file with gap name
+    for i in range(len(prefixFiles)):
+        mo = re.search(fileReg, prefixFiles[i])
+        if int(mo.group(2)) == i + 1:
+            continue
+        if i + 1 < 10:
+            shutil.copy(prefixFiles[i], prefix + '00' + str(i + 1) + '.txt')
+            os.unlink(prefixFiles[i])
+        elif i + 1 < 100:
+            shutil.copy(prefixFiles[i], prefix + '0' + str(i + 1) + '.txt')
+            os.unlink(prefixFiles[i])
+        else:
+            shutil.copy(prefixFiles[i], prefix + str(i + 1) + '.txt')
+            os.unlink(prefixFiles[i])
+
+            
