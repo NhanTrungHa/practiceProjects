f1dc8ddf1e1ade0d67c317c73b8ef64bf35fac65
Nhan Ha <nhantrungha@gmail.com>
2020-04-30 18:15:03 -0700

    Update deleteLargeFiles.py

--- deleteLargeFiles.py
+++ deleteLargeFiles.py
@@ -3,7 +3,11 @@
 
 import shutil
 import os
-
+import send2trash
 
 def deleteLargeFiles(folder):
     folder = os.path.abspath(folder)
+    for foldername, subfolders, filenames in os.walk(folder):
+        for filename in filenames:
+            if os.path.getsize(filename) >= 100000000:
+                send2trash.send2trash('filename')
