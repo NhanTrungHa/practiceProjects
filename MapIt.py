a3237327088db4e767e6689d00d6d868911166ba
Nhan Ha <nhantrungha@gmail.com>
2020-05-06 17:57:08 -0700

    new file

--- MapIt.py
+++ MapIt.py
@@ -3,7 +3,12 @@
 
 import webbrowser
 import sys
+import pyperclip
 
 if len(sys.argv) > 1:
     address = ' '.join()[1:]
     # stores address of list elements into variable and adds a space
+else:
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
---
+++ searchpypi.py
@@ -0,0 +1,17 @@
+#! python3
+# searchpypi.py - Opens top search results
+
+import requests, sys, webbrowser, bs4
+print("Searching...")
+res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+# Open Browser Tab for each result.
+linkElems = soup.select('.package-snippet')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+    print('Opening', urlToOpen)
+    webbrowser.open(urlToOpen)
