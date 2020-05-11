501b9dee0b970af83f7446f1431df841defbf1eb
Nhan Ha <nhantrungha@gmail.com>
2020-05-11 15:36:26 -0700

    Update linkVerificatoin.py

--- linkVerificatoin.py
+++ linkVerificatoin.py
@@ -1,2 +1,27 @@
 #! python3
-# linkVerificatoin.py - Opens up website and downloads every linked page and checks if error.
+# linkVerification.py - Opens up website and downloads every linked page and checks if error.
+
+import bs4
+import requests
+
+# TODO: Get URL of page.
+url = 'https://automatetheboringstuff.com/'
+print("Downloading page %s..." % url)
+
+# TODO: Attempt to download the request of the page
+res = requests.get(url)
+res.raise_for_status()
+
+# TODO: Go to given url and make list of every link
+soup = bs4.BeautifulSoup(res.text, "html.parser")
+links = soup.find_all("a")
+
+# TODO: Check if 404 status code or errors
+for link in links:
+    res = requests.get(link)
+    # TODO: If no error, continue. If error, print current url.
+    try:
+        res.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        if res.status_code == 404:
+            print("Error with link %s" % link)
